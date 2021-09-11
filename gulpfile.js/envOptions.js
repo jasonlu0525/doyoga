44d3e53552fda3d5c3dd665c4cc94d299b27e121
jaosnlu <s099113302@gmail.com>
2021-09-11 22:27:58 +0800

    add date=picker

--- gulpfile.js/envOptions.js
+++ gulpfile.js/envOptions.js
@@ -50,7 +50,6 @@ let envOptions = {
     src: [
       `${nodePath}/jquery/dist/jquery.slim.min.js`,
       `${nodePath}/bootstrap/dist/js/bootstrap.bundle.min.js`, // 已包含 popper.js
-     
     ],
     concat: 'vendors.js',
     path: `${distPath}/assets/js`,
